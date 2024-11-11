d8513a56e4e9d72c0debd54b2be243e8ca93a0e6
Avigail Shklovsky <9013825@gmail.com>
2024-11-11 10:06:35 +0200

    fix again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <p className="flex justify-center font-bold ">hello</p>
+      <p className="flex justify-center font-bold ">hello world</p>
     </>
   );
 }
